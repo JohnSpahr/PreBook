da9b6db58e10f9c680333acc77303ddac9b02a5d
JohnS <37165173+JohnSpahr@users.noreply.github.com>
2020-05-02 17:32:09 -0400

    v1.5.1
    I have made some small improvements to the code.

--- PreBook v1.5.1/app/assistants/first-assistant.js
+++ PreBook v1.5.1/app/assistants/first-assistant.js
@@ -1,20 +1,8 @@
 function FirstAssistant() {
-    /* this is the creator function for your scene assistant object. It will be passed all the 
-       additional parameters (after the scene name) that were passed to pushScene. The reference
-       to the scene controller (this.controller) has not be established yet, so any initialization
-       that needs the scene controller should be done in the setup function below. */
+
 }
 
 FirstAssistant.prototype.setup = function() {
-    /* this function is for setup tasks that have to happen when the scene is first created */
-
-    /* use Mojo.View.render to render view templates and add them to the scene, if needed */
-
-    /* setup widgets here */
-
-    /* add event handlers to listen to events from widgets */
-
-    //sets up browser and buttons
     this.browserAtt = {
         url: "http://mbasic.facebook.com",
         minFontSize: 16
@@ -94,41 +82,35 @@ FirstAssistant.prototype.setup = function() {
         ]
     };
 
-    this.controller.setupWidget(Mojo.Menu.appMenu, this.menuAttr, this.appMenuModel);
+    this.controller.setupWidget(Mojo.Menu.appMenu, this.menuAttr, this.appMenuModel); //set up app menu
 };
 
 
-FirstAssistant.prototype.activate = function(event) {
-    /* put in event handlers here that should only be in effect when this scene is active. For
-       example, key handlers that are observing the document */
-};
+FirstAssistant.prototype.activate = function(event) {};
 
 FirstAssistant.prototype.deactivate = function(event) {
-    /* remove any event handlers you added in activate and do any other cleanup that should happen before
-       this scene is popped or another scene is pushed on top */
     Mojo.Event.stopListening(this.controller.get('MyList'), Mojo.Event.listTap, this.tapHandler);
 };
 
 FirstAssistant.prototype.cleanup = function(event) {
-    /* this function should do any cleanup needed before the scene is destroyed as 
-       a result of being popped off the scene stack */
+
 };
 
 FirstAssistant.prototype.handleButtonPress1 = function(event) {
     this.controller.get('browser').mojo.openURL("http://mbasic.facebook.com/"); //loads basic version of Facebook
-}
+};
 
 FirstAssistant.prototype.handleStartLoading = function(event) {
     //show loading spinner
     this.controller.get('loadingSpinner').visible = true;
     this.controller.get('loadingSpinner').mojo.start();
-}
+};
 
 FirstAssistant.prototype.handleStopLoading = function(event) {
     //hide loading spinner
     this.controller.get('loadingSpinner').visible = false;
     this.controller.get('loadingSpinner').mojo.stop();
-}
+};
 
 FirstAssistant.prototype.handleCommand = function(inEvent) {
 
---
+++ PreBook v1.5.1/app/assistants/second-assistant.js
@@ -0,0 +1,19 @@
+function SecondAssistant() {
+
+}
+
+SecondAssistant.prototype.setup = function() {
+
+};
+
+SecondAssistant.prototype.activate = function(event) {
+
+};
+
+SecondAssistant.prototype.deactivate = function(event) {
+
+};
+
+SecondAssistant.prototype.cleanup = function(event) {
+
+};
