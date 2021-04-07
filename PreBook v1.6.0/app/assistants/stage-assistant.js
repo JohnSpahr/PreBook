88681eb7e3097ca40121908272f988173f0417b8
JohnS <37165173+JohnSpahr@users.noreply.github.com>
2021-04-06 20:50:26 -0400

    surprise update!
    going to add the auto updater so not complete yet

---
+++ PreBook v1.6.0/app/assistants/stage-assistant.js
@@ -0,0 +1,35 @@
+function StageAssistant() {
+	/* this is the creator function for your stage assistant object */
+}
+
+StageAssistant.prototype.setup = function() {
+	/* this function is for setup tasks that have to happen when the stage is first created */
+	this.controller.setWindowOrientation("free");	
+	Mojo.Controller.stageController.pushScene("first");
+}
+
+StageAssistant.prototype.handleCommand = function(inEvent) {
+ 
+  switch (inEvent.type) {
+ 
+    case Mojo.Event.commandEnable:
+      switch (inEvent.command) {
+        case Mojo.Menu.helpCmd:
+          inEvent.stopPropagation();
+        break;
+      }
+    break;
+    
+    case Mojo.Event.command:
+      switch (inEvent.command) {
+        case Mojo.Menu.helpCmd:     
+          this.controller.pushAppSupportInfoScene();
+        break;     
+      }
+    break;
+	
+	
+    
+  }
+  
+};
