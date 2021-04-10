76f8c57f302caf7cf1627ebdf543e163410eda12
JohnS <37165173+JohnSpahr@users.noreply.github.com>
2021-04-10 18:36:50 -0400

    1.6.0 is complete! Enjoy!

--- PreBook/app/assistants/first-assistant.js
+++ PreBook/app/assistants/first-assistant.js
@@ -1,8 +1,31 @@
-function FirstAssistant() {
+/*
+______        ______             _    
+| ___ \       | ___ \           | |   
+| |_/ / __ ___| |_/ / ___   ___ | | __
+|  __/ '__/ _ \ ___ \/ _ \ / _ \| |/ /
+| |  | | |  __/ |_/ / (_) | (_) |   < 
+\_|  |_|  \___\____/ \___/ \___/|_|\_\
+                                      
+                                      
+Copyright (c) John Spahr 2019-2021
+*/
 
+function FirstAssistant() {
+    this.updateCheckDone = false;
 }
 
 FirstAssistant.prototype.setup = function() {
+    this.updaterModel = new UpdaterModel();
+
+    //check if first run
+    var currVersion = Mojo.Controller.appInfo.version;
+    var firstRun = new Mojo.Model.Cookie("firstRun" + currVersion);
+    if (firstRun.get() == undefined) {
+        //if first run, show version info
+        firstRun.put(false);
+        Mojo.Controller.stageController.pushScene("second");
+    }
+
     this.browserAtt = {
         url: "http://mbasic.facebook.com",
         cacheAdapter: true,
@@ -80,7 +103,7 @@ FirstAssistant.prototype.setup = function() {
             { label: "Notes", command: "notes", shortcut: "t" },
             { label: "Settings/Privacy", command: "settings" },
             { label: "App Info" },
-            { label: "Change Log", command: "whatsNew" },
+            { label: "Version Info", command: "whatsNew" },
             Mojo.Menu.helpItem
         ]
     };
@@ -88,7 +111,17 @@ FirstAssistant.prototype.setup = function() {
     this.controller.setupWidget(Mojo.Menu.appMenu, this.menuAttr, this.appMenuModel); //set up app menu
 };
 
-FirstAssistant.prototype.activate = function(event) {};
+FirstAssistant.prototype.activate = function(event) {
+    this.updateCheckDone = true;
+    this.updaterModel.CheckForUpdate("PreBook", function(responseObj) {
+        if (responseObj && responseObj.updateFound) {
+            this.updaterModel.PromptUserForUpdate(function(response) {
+                if (response)
+                    this.updaterModel.InstallUpdate();
+            }.bind(this));
+        }
+    }.bind(this));
+};
 
 FirstAssistant.prototype.deactivate = function(event) {
     Mojo.Event.stopListening(this.controller.get('MyList'), Mojo.Event.listTap, this.tapHandler);
@@ -99,7 +132,19 @@ FirstAssistant.prototype.cleanup = function(event) {
 };
 
 FirstAssistant.prototype.handleHeaderPress = function(event) {
-    this.controller.get('browser').mojo.openURL("http://mbasic.facebook.com/"); //loads basic version of Facebook
+    this.controller.showAlertDialog({
+        onChoose: function(value) {
+            if (value == "yes") {
+                this.controller.get('browser').mojo.openURL("http://mbasic.facebook.com/"); //go home
+            }
+        },
+        title: "Go Home?",
+        message: "This will take you to the Facebook homepage.",
+        choices: [
+            { label: "Yes", value: "yes", type: "affirmative" },
+            { label: "No", value: "no", type: "negative" }
+        ]
+    });
 };
 
 FirstAssistant.prototype.handleStartLoading = function(event) {
@@ -165,7 +210,7 @@ FirstAssistant.prototype.handleCommand = function(inEvent) {
             break;
 
         case "whatsNew":
-            Mojo.Controller.stageController.pushScene("second"); //change log
+            Mojo.Controller.stageController.pushScene("second"); //version info
             break;
 
         case "settings":
@@ -179,6 +224,7 @@ FirstAssistant.prototype.handleCommand = function(inEvent) {
         case "notes":
             this.controller.get('browser').mojo.openURL("https://mbasic.facebook.com/notes"); //facebook notes
             break;
+
         case "covidUpdate":
             this.controller.get('browser').mojo.openURL("https://mbasic.facebook.com/coronavirus_info"); //COVID-19 Updates
             break;
--- PreBook/app/assistants/second-assistant.js
+++ PreBook/app/assistants/second-assistant.js
@@ -1,20 +1,36 @@
+/*
+______        ______             _    
+| ___ \       | ___ \           | |   
+| |_/ / __ ___| |_/ / ___   ___ | | __
+|  __/ '__/ _ \ ___ \/ _ \ / _ \| |/ /
+| |  | | |  __/ |_/ / (_) | (_) |   < 
+\_|  |_|  \___\____/ \___/ \___/|_|\_\
+                                      
+                                      
+Copyright (c) John Spahr 2019-2021
+*/
+
 function SecondAssistant() {
 
 }
 
 SecondAssistant.prototype.setup = function() {
-    this.model = {
-        buttonLabel: "Close"
+    this.commandMenuModel = {
+        items: [{
+                items: []
+            },
+            {
+                items: [
+                    { label: $L('Close'), command: "closeScene" }
+                ]
+            },
+            {
+                items: []
+            }
+        ]
     };
-    this.controller.setupWidget('closeBtn', {}, this.model);
 
-    //add a listener for closeBtn
-    Mojo.Event.listen(this.controller.get("closeBtn"), Mojo.Event.tap, this.handleCloseTap.bind(this));
-};
-
-SecondAssistant.prototype.handleCloseTap = function(event) {
-    //close scene
-    Mojo.Controller.stageController.popScene();
+    this.controller.setupWidget(Mojo.Menu.commandMenu, undefined, this.commandMenuModel);
 };
 
 SecondAssistant.prototype.activate = function(event) {
@@ -27,4 +43,13 @@ SecondAssistant.prototype.deactivate = function(event) {
 
 SecondAssistant.prototype.cleanup = function(event) {
 
+};
+
+SecondAssistant.prototype.handleCommand = function(inEvent) {
+    switch (inEvent.command) {
+        case "closeScene":
+            //close scene
+            Mojo.Controller.stageController.popScene();
+            break;
+    }
 };
